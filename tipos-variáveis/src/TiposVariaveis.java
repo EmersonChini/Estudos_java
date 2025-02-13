02564cd7caa968391c90d72c2b4281bbdac06547
Emerson Santos <emersonsermais@outlook.com>
2025-02-12 22:22:03 -0300

    Atualizações variáveis

---
+++ tipos-variáveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salario = 1270.50;
+        short numeroCurto = 1;
+        int numeroInteiro = 1;
+        short numero2 = (short) numeroInteiro;
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double Valor_De_Pi = 3.14; 
+        
+    }
+}
